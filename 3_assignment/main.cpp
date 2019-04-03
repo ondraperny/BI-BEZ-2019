975cfec607ab990584031e3f70f0c9bcffc661eb
Ondřej Perný <pernyond@fit.cvut.cz>
2019-04-03 23:52:07 +0200

    third assignment reworked

--- 3_assignment/main.cpp
+++ 3_assignment/main.cpp
@@ -12,176 +12,102 @@
 
 void decryption() {
 
-
 }
 
-void encryption() {
-//    int res;
-//    unsigned char ot1[1024] = "abcdefghijklmnopqrstuvwxyz0123";  // open text
-//    unsigned char ot2[1024] = "Druha zpravaDruha zprava-konec";  // open text
-//    unsigned char st1[1024];  // sifrovany text
-//    unsigned char st2[1024];  // sifrovany text 2
-//    unsigned char key[EVP_MAX_KEY_LENGTH] = "Muj klic";  // klic pro sifrovani
-//    unsigned char iv[EVP_MAX_IV_LENGTH] = "inicial. vektor";  // inicializacni vektor
-//    const char cipherName[] = "RC4";
-//    const EVP_CIPHER *cipher;
-//
-//    OpenSSL_add_all_ciphers();
-//    /* sifry i hashe by se nahraly pomoci OpenSSL_add_all_algorithms() */
-//    cipher = EVP_get_cipherbyname(cipherName);
-//    if (!cipher) {
-//        printf("Sifra %s neexistuje.\n", cipherName);
-//        exit(1);
-//    }
-//
-//    int otLength = strlen((const char *) ot1);
-//    int stLength = 0;
-//    int tmpLength = 0;
-//
-//    EVP_CIPHER_CTX *ctx; // context structure
-//    ctx = EVP_CIPHER_CTX_new();
-//    if (ctx == NULL) exit(2);
-//
-//    printf("OT : %s\n", ot1);
-//    /* Sifrovani */
-//    res = EVP_EncryptInit_ex(ctx, cipher, NULL, key, iv);  // context init - set cipher, key, init vector
-//    if (res != 1) exit(3);
-//    res = EVP_EncryptUpdate(ctx, st1, &tmpLength, ot1, otLength);  // encryption of pt
-//
-//    if (res != 1) exit(4);
-//    stLength += tmpLength;
-//    res = EVP_EncryptFinal_ex(ctx, st1 + stLength, &tmpLength);  // get the remaining ct
-//    if (res != 1) exit(5);
-//    stLength += tmpLength;
-//
-//    std::cout << "ST1: ";
-//    for (int i = 0; i < stLength; i++)
-//        std::cout  << std::setw(2) << std::setfill('0') << std::hex << (unsigned int) st1[i];
-//    std::cout << std::endl;
-//
-//    otLength = strlen((const char *) ot2);
-//    stLength = 0;
-//    tmpLength = 0;
-//
-//    /* Sifrovani */
-//    res = EVP_EncryptInit_ex(ctx, cipher, NULL, key, iv);  // context init - set cipher, key, init vector
-//    if (res != 1) exit(3);
-//    res = EVP_EncryptUpdate(ctx, st2, &tmpLength, ot2, otLength);  // encryption of pt
-//    if (res != 1) exit(4);
-//    stLength += tmpLength;
-//    res = EVP_EncryptFinal_ex(ctx, st2 + stLength, &tmpLength);  // get the remaining ct
-//    if (res != 1) exit(5);
-//    stLength += tmpLength;
-//
-//    std::cout << "ST2: ";
-//    for (int i = 0; i < stLength; i++)
-//        std::cout  << std::setw(2) << std::setfill('0') << std::hex << (unsigned int) st2[i];
-//    std::cout << std::endl;
-//
-//    /* Clean up */
-//    EVP_CIPHER_CTX_free(ctx);
-}
-
-int main(int argc, char *argv[]) {
-//    if ((argc != 3) || ((std::string(argv[1]) != "-e") && ((std::string(argv[1]) != "-d")))) {
-//        std::cout << "Spatne volani zvolte jeden prepinac (-e nebo -d)" << std::endl;
-//        return 0;
-//    }
-//    if ((std::string(argv[2]) != "ecb") && ((std::string(argv[2]) != "cbc"))) {
-//        std::cout << "Spatne volani zvolte jeden mod (ebc nebo cbc)" << std::endl;
-//        return 0;
-//    }
-//
-//    if (std::string(argv[1]) == "-e")
-//        encryption();
-//    if (std::string(argv[1]) == "-d")
-//        decryption();
-
-    std::string filename = "C:\\Users\\ondra\\Desktop\\BEZ\\3_assignment\\Mad_scientist.bmp";
-    // open the file:
-    std::ifstream file(filename, std::ios::binary);
-
-//    std::cout << file.fail() << std::endl;
-
-    // Stop eating new lines in binary mode!!!
-    file.unsetf(std::ios::skipws);
-
-    // get its size:
-    std::streampos fileSize;
-
-    file.seekg(0, std::ios::end);
-    fileSize = file.tellg();
-    file.seekg(0, std::ios::beg);
 
 
+class File {
+    std::ifstream input;
+    std::ofstream output;
+    long start, size;
+    std::string inputName, outputName, actionType, cipherType;
 
-//////////////////////////////////////////////////////////////////////////////
+public:
+    void inicialize(const std::string & action, const std::string & cipher){
+        input.open(inputName, std::ios::binary);
+        if (input.fail()){
+            std::cout << "Nepodarilo se otevrit vstupni soubor" << std::endl;
+            exit(1);
+        }
+        output.open(outputName, std::ios::binary );
+        start = read4bytes(10);
+        size = read4bytes(2);
+        this->actionType = action;
+        this->cipherType = cipher;
+        std::cout << "Start: " << std::dec << start << "...Size: " << std::dec << size << std::endl;
 
-//    // reserve capacity
-    std::vector<unsigned char> vec;
-    vec.reserve(fileSize);
-    // read the data:
-    vec.insert(vec.begin(),
-               std::istream_iterator<unsigned char>(file),
-               std::istream_iterator<unsigned char>());
-
-
-//    std::cout << "Obsah souboru: " << std::endl;
-//    int cnt = 0;
-//    for(const char & character : vec) {
-////        std::cout << std::hex << (unsigned int)character << " ";
-//        std::cout << std::setw(10) << std::setfill(' ') << (unsigned int)character << " ";
-//        if ((cnt % 10) == 0)
-//            std::cout << std::endl;
-//        cnt++;
-//    }
-
-
-    std::string tmp;
-
-    tmp.push_back(vec[2]);
-    tmp.push_back(vec[3]);
-    tmp.push_back(vec[4]);
-    tmp.push_back(vec[5]);
-
-
-//    std::cout << "Size: ";
-//    for(size_t i = 3; i <= 0; i++) {
-//        std::cout << std::hex << (unsigned int)tmp[i] << " ";
-//    }
-
-//    std::cout << std::endl;
-
-    std::ofstream out("output.bmp");
-
-    std::cout << vec.size() << " " << fileSize << std::endl;
-    for(size_t i = 0; i < vec.size(); i++) {
-        out << vec[i];
     }
 
-    out.close();
+    File() {
+        inputName = "Mad_scientist.bmp";
+        outputName = "Mad_scientist_ebc.bmp";
+    }
 
-    long size = long((unsigned char)(vec[2]) << 24 |
-                     (unsigned char)(vec[3]) << 16 |
-                     (unsigned char)(vec[4]) << 8 |
-                     (unsigned char)(vec[5]));
+    ~File() {
+        input.close();
+        output.close();
+    }
 
-    long begin = long((unsigned char)(vec[10]) << 24 |
-                      (unsigned char)(vec[11]) << 16 |
-                      (unsigned char)(vec[12]) << 8 |
-                      (unsigned char)(vec[13]));
+    void copyHeader() {
+        input.seekg(0);
+        char * header = new char[this->start];
+        input.read(header, start);
+        output.write(header, start);
+    }
 
-//    std::cout << "Size: ";
-//    for(size_t i = 0; i <= 3; i++) {
-//        std::cout << std::hex << (unsigned int)size[i] << " ";
-//    }
-//    std::cout << std::endl;
+    void copyBody() {
+       char * body = new char[this->size - this->start];
+       input.seekg(start);
+       input.read(body, this->size - this->start);
+       output.write(body, this->size - this->start);
+   }
 
-    std::cout << "Size : " << std::dec << size << std::endl;
-    std::cout << "Begin: " << std::dec << begin<< std::endl;
+    long read4bytes(unsigned int from) {
+        char * tmp = new char[4];
+        input.seekg(from);
+        input.read(tmp, 4);
+        std::stringstream iss;
+        for (int i = 3; i >= 0; --i) {
+            iss << std::setw(2) << std::setfill('0') << std::hex << (unsigned int)(unsigned char)tmp[i] << std::flush;
+            std::cout << std::setw(2) << std::setfill('0') << std::hex << (unsigned int)(unsigned char)tmp[i] << " " <<std::flush;
+        }
+        std::cout << iss.str() << std::endl;
+        return std::stol(iss.str(), nullptr, 16);
+    }
 
+    void encryption() {
+        unsigned char key[EVP_MAX_KEY_LENGTH] = "Muj tajny klic";  // klic pro sifrovani
+        unsigned char iv[EVP_MAX_IV_LENGTH] = "inicial. vektor";  // inicializacni vektor
 
-    std::cout << std::endl;
+        OpenSSL_add_all_ciphers();
+        int stLength = 0;
+        int tmpLength = 0;
+
+        EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+
+        if(std::string{argv[2]} == "ecb"){
+            EVP_EncryptInit(ctx, EVP_aes_128_ecb(), key, iv);
+        } else {
+            EVP_EncryptInit(ctx, EVP_aes_128_cbc(), key, iv);
+        }
+
+        EVP_EncryptUpdate(ctx,  out_img, &stLength, img, length);  // sifrovani ot1
+        free(img);
+
+        EVP_EncryptFinal(ctx, out_img + stLength, &tmpLength);  // dokonceni (ziskani zbytku z kontextu)
+        stLength += tmpLength;
+
+        EVP_CIPHER_CTX_free(ctx);
+    }
+
+};
+
+int main(int argc, char *argv[]) {
+    File file;
+    file.inicialize("enc", "cbc");
+    file.copyHeader();
+    file.copyBody();
+
 
 
     return 0;
